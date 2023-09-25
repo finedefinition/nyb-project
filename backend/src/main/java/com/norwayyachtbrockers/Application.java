ecb073d31308763cb0404ff6305e33399a12d1ef
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-09-25 23:09:33 +0100

    Develope Application.class

---
+++ backend/src/main/java/com/norwayyachtbrockers/Application.java
@@ -0,0 +1,16 @@
+package com.norwayyachtbrockers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@PropertySource("file:${user.dir}/backend/.env")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.norwayyachtbrockers.Application.class, args);
+    }
+}
