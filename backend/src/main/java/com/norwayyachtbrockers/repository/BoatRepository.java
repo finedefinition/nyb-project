852be416fad469914e628b7ab9bd8e30949b0491
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-09-27 20:42:05 +0100

    Resolve maven parent dependencies

--- backend/src/main/java/com/norwayyachtbrockers/repository/BoatRepository.java
+++ backend/src/main/java/com/norwayyachtbrockers/repository/BoatRepository.java
@@ -1,14 +1,12 @@
 package com.norwayyachtbrockers.repository;
 
 import com.norwayyachtbrockers.model.Boat;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface BoatRepository extends JpaRepository<Boat, Long> {
-
 
     List<Boat> findByBoatName(String boatName);
 }
