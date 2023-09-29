a80666afabe6216f67b71e2fb3d4fb5a7d14ad55
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-09-29 10:44:28 +0100

    Add created_at column

--- backend/src/main/java/com/norwayyachtbrockers/config/DataInitializer.java
+++ backend/src/main/java/com/norwayyachtbrockers/config/DataInitializer.java
@@ -4,6 +4,9 @@ import com.norwayyachtbrockers.model.Boat;
 import com.norwayyachtbrockers.service.BoatServiceImpl;
 import jakarta.annotation.PostConstruct;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,7 +19,8 @@ public class DataInitializer {
 
     @PostConstruct
     public void inject() {
-        Boat boatOne = new Boat("Serenity", new BigDecimal(180000), "Amel", 2016);
+        Boat boatOne = new Boat("Serenity", new BigDecimal(180000),
+                "Amel", 2016, LocalDateTime.now(Clock.systemUTC()));
         boatService.save(boatOne);
     }
 }
