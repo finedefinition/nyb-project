bf0fe463b1e34254e9f372ed7e9485f63d242abd
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-09-29 08:45:53 +0100

    Add .yml files for Railway v 21.0

--- backend/src/main/java/com/norwayyachtbrockers/controler/IndexController.java
+++ backend/src/main/java/com/norwayyachtbrockers/controler/IndexController.java
@@ -10,6 +10,22 @@ public class IndexController {
     @GetMapping("/")
     @ResponseBody
     public String hello() {
-        return "Hello kids! Hello captain!";
+        return "Are you ready, kids?\n" +
+                "Aye, aye, Captain!\n" +
+                "I can't hear you!\n" +
+                "Aye, aye, Captain!\n" +
+                "Oh!\n" +
+                "\n" +
+                "Who lives in a pineapple under the sea?\n" +
+                "SpongeBob SquarePants!\n" +
+                "Absorbent and yellow and porous is he\n" +
+                "SpongeBob SquarePants!\n" +
+                "If nautical nonsense be something you wish\n" +
+                "SpongeBob SquarePants!\n" +
+                "Then drop on the deck and flop like a fish!\n" +
+                "SpongeBob SquarePants! (Ready?!)\n" +
+                "SpongeBob SquarePants!\n" +
+                "SpongeBob SquarePants!\n" +
+                "SpongeBob SquarePants!";
     }
 }
