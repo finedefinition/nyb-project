94c4a0e935314a8117bb1fece37746203ff582b0
MargaritaChorniak <chorniak.rita@gmail.com>
2023-10-04 17:25:08 +0300

    added part of the prototype

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+import { SectionCard } from './components/SectionCard';
+
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      {/* <GoodList goods={goods}/> */}
+      <SectionCard />
+    </>
+  )
+}
