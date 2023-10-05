049e8aed2218282766caad3a4bf8b1c9550940ab
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-10-05 14:17:50 +0100

    Minor frontend changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -9,22 +9,22 @@ export const Header = () => {
     <nav className="header__nav">
       <ul className="header__nav__item">
         <li className="header__nav__list">
-          <a href="/" className="header__nav__link"> Про нас</a>
+          <a href="/" className="header__nav__link">Yachts</a>
         </li>
         <li className="header__nav__list">
-          <a href="/" className="header__nav__link">Каталог</a>
+          <a href="/" className="header__nav__link">How it works?</a>
         </li>
         <li className="header__nav__list">
-        <a href="/" className="header__nav__link">Етапи</a>
+        <a href="/" className="header__nav__link">Services</a>
         </li>
         <li className="header__nav__list">
-          <a href="/" className="header__nav__link">Контакти</a>
+          <a href="/" className="header__nav__link">About us</a>
         </li>
       </ul>
     </nav>
     <div className="header__buttons">
-      <button className="header__btn">Вхід</button>
-      <button className="header__btn">Реєстрація</button>
+      <button className="header__btn">Sign In</button>
+      <button className="header__btn">Sign Up</button>
     </div>
   </header>
   )
