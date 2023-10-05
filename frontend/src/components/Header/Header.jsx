42c7be91727572617c92cc2b9393a233d56c1b8a
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-10-05 17:16:44 +0100

    Set GitHub pages v 2.0

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,21 +4,21 @@ export const Header = () => {
   return (
     <header className="header">
     <div className="header__logo">
-      <a href="/" className='logo'>LOGO</a>
+      <a href="https://finedefinition.github.io/nyb-project/" className='logo'>LOGO</a>
     </div>
     <nav className="header__nav">
       <ul className="header__nav__item">
         <li className="header__nav__list">
-          <a href="/" className="header__nav__link">Yachts</a>
+          <a href="https://finedefinition.github.io/nyb-project/" className="header__nav__link">Yachts</a>
         </li>
         <li className="header__nav__list">
-          <a href="/" className="header__nav__link">How it works?</a>
+          <a href="https://finedefinition.github.io/nyb-project/" className="header__nav__link">How it works?</a>
         </li>
         <li className="header__nav__list">
-        <a href="/" className="header__nav__link">Services</a>
+        <a href="https://finedefinition.github.io/nyb-project/" className="header__nav__link">Services</a>
         </li>
         <li className="header__nav__list">
-          <a href="/" className="header__nav__link">About us</a>
+          <a href="https://finedefinition.github.io/nyb-project/" className="header__nav__link">About us</a>
         </li>
       </ul>
     </nav>
