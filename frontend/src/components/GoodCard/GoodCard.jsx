de0bff4d658182f50588b07baebd42ccd2beac2e
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-10-05 15:21:36 +0100

    Set Amplify  v 3.0

--- frontend/src/components/GoodCard/GoodCard.jsx
+++ frontend/src/components/GoodCard/GoodCard.jsx
@@ -3,7 +3,7 @@ import './GoodCard.css';
 export const GoodCard = ({ good }) => {
   return (
     <div className="GoodCard">
-          <img src="/src/Images/GoodCard/yacht.jpg" alt="Яхта" className="GoodCard__img" />
+          <img src="/src/Images/GoodCard/yacht.jpg" alt="Boat" className="GoodCard__img" />
           <h3 className="GoodCard__name">{good.name}</h3>
           <div className="GoodCard__price">${good.price}</div>
           <div className="GoodCard__place">{good.place}</div>
