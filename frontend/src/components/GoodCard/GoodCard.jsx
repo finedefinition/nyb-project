f18d5cdab91e2a11be00c04903d6fa9eb59ad413
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-10-05 18:40:14 +0100

    Prototype minor changes

--- frontend/src/components/GoodCard/GoodCard.jsx
+++ frontend/src/components/GoodCard/GoodCard.jsx
@@ -1,12 +1,16 @@
 import './GoodCard.css';
 
-export const GoodCard = ({ good }) => {
-  return (
-    <div className="GoodCard">
-          <img src="/src/Images/GoodCard/yacht.jpg" alt="Boat" className="GoodCard__img" />
-          <h3 className="GoodCard__name">{good.name}</h3>
-          <div className="GoodCard__price">${good.price}</div>
-          <div className="GoodCard__place">{good.place}</div>
-    </div>
-  )
+export const GoodCard = ({good}) => {
+    return (
+        <div className="GoodCard">
+            <img
+                src={`/Images/SectionCard/${good.imageUrl}`} // Use a relative path starting with `/` and interpolate `good.imageUrl`
+                alt="Boat"
+                className="GoodCard__img"
+            />
+            <h3 className="GoodCard__name">{good.name}</h3>
+            <div className="GoodCard__place">{good.place} | {good.year} </div>
+            <div className="GoodCard__price"><b>€{good.price}</b></div>
+        </div>
+    )
 }
--- frontend/src/components/GoodList/GoodList.jsx
+++ frontend/src/components/GoodList/GoodList.jsx
@@ -4,9 +4,9 @@ import './GoodList.css';
 export const GoodList = ({ goods }) => {
   return (
     <div className="GoodList">
-        {goods.map(good => (
-          <GoodCard good={good} key={good.id}/>
-        ))}
+      {goods.map((good) => (
+        <GoodCard good={good} key={good.id} />
+      ))}
     </div>
-  )
-}
+  );
+};
