6667faa9f9fd9e1f57d4def7d5a62b5a1346c137
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-10-09 14:08:35 +0100

    Add CORS configuration

--- backend/src/main/java/com/norwayyachtbrockers/controler/BoatController.java
+++ backend/src/main/java/com/norwayyachtbrockers/controler/BoatController.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/boats")
 public class BoatController {
     private final BoatService boatService;
--- frontend/src/components/SectionCard/SectionCard.jsx
+++ frontend/src/components/SectionCard/SectionCard.jsx
@@ -1,15 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Storage } from 'aws-amplify';
 import { GoodList } from '../GoodList';
 import './SectionCard.css';
-import goods from './goods.json';
 
 export const SectionCard = () => {
+  const [featuredBoats, setFeaturedBoats] = useState([]);
+
+  useEffect(() => {
+    fetch('https://nyb-project-production.up.railway.app/boats/featured')
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(async (data) => {
+          // Fetch the images for each boat using Amplify Storage
+          const boatsWithImages = await Promise.all(
+              data.map(async (boat) => {
+                try {
+                  const imageUrl = await Storage.get(boat.imageUrl);
+                  return { ...boat, imageUrl };
+                } catch (error) {
+                  console.error('Error fetching image from S3:', error);
+                  return boat; // Keep the boat data even if image retrieval fails
+                }
+              })
+          );
+
+          // Set the updated data in the state
+          setFeaturedBoats(boatsWithImages);
+        })
+        .catch((error) => {
+          console.error('Error fetching featured boats:', error);
+        });
+  }, []);
+
   return (
-    <section className="section">
-    <div className="section__up">
-      <h2 className='section__title'>Featured yachts</h2>
-      <button className='section__btn'>See all</button>
-    </div>
-      <GoodList goods={goods}/>
-    </section>
-  )
-}
+      <section className="section">
+        <div className="section__up">
+          <h2 className="section__title">Featured yachts</h2>
+          <button className="section__btn">See all</button>
+        </div>
+        <GoodList goods={featuredBoats} />
+      </section>
+  );
+};
