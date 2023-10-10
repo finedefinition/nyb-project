18a1231013870a34e9e25f8391ad08f7c81750ed
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-10-10 11:09:15 +0100

    Frontend: hide S3 settings in config.json

--- frontend/src/components/GoodCard/GoodCard.jsx
+++ frontend/src/components/GoodCard/GoodCard.jsx
@@ -2,41 +2,38 @@ import React, { useState, useEffect } from 'react';
 import AWS from 'aws-sdk';
 import './GoodCard.css';
 
-
 export const GoodCard = ({ good }) => {
-
     const [imageUrl, setImageUrl] = useState(null);
 
     useEffect(() => {
-        // Configure AWS SDK with your credentials and S3 bucket information
-
-
-        AWS.config.update({
-            accessKeyId: 'AKIAUXLH7DAVIEHB5FF2',
-            secretAccessKey: 'N7ARg0AZ02niNCLZznIUA3VUs0fC2we761Mz4Cwn',
-            region: 'eu-north-1', // Change to your desired AWS region
-        });
-
-        const s3 = new AWS.S3();
-
-        const bucketName = 'nyb-basket';
-        // Fetch the image from Amazon S3 based on the imageUrl from the good prop
-        async function fetchImageFromS3() {
+        async function fetchConfig() {
             try {
-                const s3Object = await s3
-                    .getObject({ Bucket: bucketName, Key: good.imageUrl })
-                    .promise();
+                const response = await fetch('/config.json');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch configuration');
+                }
+                const config = await response.json();
+
+                // Configure AWS SDK with the credentials and region
+                AWS.config.update({
+                    accessKeyId: config.S3_KEY_ID,
+                    secretAccessKey: config.S3_KEY_SECRET,
+                    region: config.S3_REGION_NAME,
+                });
+
+                const s3 = new AWS.S3();
+                const bucketName = 'nyb-basket';
+
+                // Fetch the image from Amazon S3 based on the imageUrl from the good prop
+                const s3Object = await s3.getObject({ Bucket: bucketName, Key: good.imageUrl }).promise();
                 const imageUrl = URL.createObjectURL(new Blob([s3Object.Body]));
                 setImageUrl(imageUrl);
             } catch (error) {
-                console.error(
-                    `Error fetching image ${good.imageUrl} from S3:`,
-                    error
-                );
+                console.error('Error:', error);
             }
         }
 
-        fetchImageFromS3();
+        fetchConfig();
 
     }, [good]);
 
