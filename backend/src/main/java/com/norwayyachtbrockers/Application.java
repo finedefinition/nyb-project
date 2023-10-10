958d9352678f4efe334e90698c71e2f851523ad4
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-10-10 11:22:39 +0100

    Minor fix v 1.0

--- backend/src/main/java/com/norwayyachtbrockers/Application.java
+++ backend/src/main/java/com/norwayyachtbrockers/Application.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@PropertySource("file:${user.dir}/.env")
+//@PropertySource("file:${user.dir}/.env")
 public class Application {
 
     public static void main(String[] args) {
