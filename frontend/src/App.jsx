25b4a1c2a864e7c1b7dce5cfb9dca3d48d618465
MargaritaChorniak <chorniak.rita@gmail.com>
2023-10-11 12:26:21 +0300

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Yachts } from './pages/Yachts';
 import { Main } from './pages/Main';
