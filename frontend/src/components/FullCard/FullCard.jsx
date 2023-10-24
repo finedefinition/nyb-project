78a65729e42117f2c86c7facc6eab9aa1bd76cbc
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-10-24 14:24:33 +0100

    Frontend: Finish Beautify and unify pages

--- frontend/src/components/FullCard/FullCard.jsx
+++ frontend/src/components/FullCard/FullCard.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
 import AWS from 'aws-sdk';
 import './FullCard.css';
 
@@ -8,56 +8,58 @@ export const FullCard = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [imageUrl, setImageUrl] = useState(null);
-    const { id } = useParams();
+    const {id} = useParams();
+
+    async function fetchData() {
+        try {
+            const response = await fetch(`https://nyb-project-production.up.railway.app/api/vessels/${id}`);
+            if (!response.ok) {
+                throw new Error(`Failed to fetch boat data: ${response.status}`);
+            }
+            const data = await response.json();
+            setBoatData(data);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    async function fetchImage() {
+        if (!boatData || !boatData.imageKey) return;
+        try {
+            const response = await fetch('/config.json');
+            if (!response.ok) {
+                throw new Error('Failed to fetch configuration');
+            }
+            const config = await response.json();
+
+            AWS.config.update({
+                accessKeyId: config.S3_KEY_ID,
+                secretAccessKey: config.S3_KEY_SECRET,
+                region: config.S3_REGION_NAME,
+            });
+
+            const s3 = new AWS.S3();
+            const bucketName = 'nyb-basket';
+
+            const s3Object = await s3.getObject({Bucket: bucketName, Key: boatData.imageKey}).promise();
+            const imageUrl = URL.createObjectURL(new Blob([s3Object.Body]));
+            setImageUrl(imageUrl);
+        } catch (error) {
+            setError(error);
+        }
+    }
 
     useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch(`https://nyb-project-production.up.railway.app/api/vessels/${id}`);
-                if (!response.ok) {
-                    throw new Error(`Failed to fetch boat data: ${response.status}`);
-                }
-                const data = await response.json();
-                setBoatData(data);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }
+        fetchData();
+    }, [id]);
+
+    useEffect(() => {
+        if (boatData) {
+            fetchImage();
         }
-
-        async function fetchImage() {
-            try {
-                if (boatData && boatData.imageKey) {
-                    const response = await fetch('/config.json');
-                    if (!response.ok) {
-                        throw  Error('Failed to fetch configuration');
-                    }
-                    const config = await response.json();
-
-                    AWS.config.update({
-                        accessKeyId: config.S3_KEY_ID,
-                        secretAccessKey: config.S3_KEY_SECRET,
-                        region: config.S3_REGION_NAME,
-                    });
-
-                    const s3 = new AWS.S3();
-                    const bucketName = 'nyb-basket';
-
-                    const s3Object = await s3.getObject({ Bucket: bucketName, Key: boatData.imageKey }).promise();
-                    const imageUrl = URL.createObjectURL(new Blob([s3Object.Body]));
-                    setImageUrl(imageUrl);
-                }
-            } catch (error) {
-                setError(error);
-            }
-        }
-
-        fetchData()
-            .then(() => fetchImage())
-            .catch((error) => setError(error))
-            .finally(() => setLoading(false));
-    }, [id, boatData]);
+    }, [boatData]);
 
     if (loading) {
         return <p className="loading-text">Loading boat data...</p>;
@@ -72,32 +74,85 @@ export const FullCard = () => {
     }
 
     return (
-        <div className="FullCard">
-            <div className="card-title">
-               <h2>{boatData.vesselMake} {boatData.vesselModel}</h2><h3><i>{boatData.vesselLocationCountry},{boatData.vesselLocationState} €{boatData.vesselPrice}</i></h3>
+        <div className="container">
+            <div className="top-section">
+                <div className="left-side"> {/* Image*/}
+                    <div className="image-container">
+                        <img src={imageUrl} alt="Vessel" className="vessel-image"/>
+                    </div>
+                </div>
+                <div className="right-side">
+                    <label>ABOUT</label>
+                    <p className="form-row">{boatData.vesselDescription}</p>
+                </div>
             </div>
-            <img src={imageUrl} alt="Boat" className="card-image" />
-            <div className="card-details">
-
-                <div className="card-info">
-                    <div className="form-columns">
-                        <div className="form-column">
-
-                        </div>
-                    <p className="card-info-item">Price: €{boatData.vesselPrice}</p>
-                    <p className="card-info-item">Year: {boatData.vesselYear}</p>
-                    <p className="card-info-item">Country: {boatData.vesselLocationCountry}</p>
-                    <p className="card-info-item">State: {boatData.vesselLocationState}</p>
-                    <p className="card-info-item">Length Overall: {boatData.vesselLengthOverall} feet</p>
-                    <p className="card-info-item">Beam: {boatData.vesselBeam} feet</p>
-                    <p className="card-info-item">Draft: {boatData.vesselDraft} feet</p>
-                    <p className="card-info-item">Cabin: {boatData.vesselCabin}</p>
-                    <p className="card-info-item">Berth: {boatData.vesselBerth}</p>
-                    <p className="card-info-item">Keel Type: {boatData.vesselKeelType}</p>
-                    <p className="card-info-item">Fuel Type: {boatData.vesselFuelType}</p>
-                    <p className="card-info-item">Engine Quantity: {boatData.engineQuantity}</p>
-                    <p className="description">{boatData.vesselDescription}</p>
-                    <p className="card-info-item">Created At: {new Date(boatData.createdAt).toLocaleString()}</p>
+            <div className="form-columns">
+                <div className="form-column">
+                    <div className="form-row">
+                        <label>MAKE</label>
+                        {boatData.vesselMake}
+                    </div>
+                    <div className="form-row">
+                        <label>LENGTH OVERALL</label>
+                        {boatData.vesselLengthOverall}
+                    </div>
+                    <div className="form-row">
+                        <label>CABIN</label>
+                        {boatData.vesselCabin}
+                    </div>
+                </div>
+                <div className="form-column">
+                    <div className="form-row">
+                        <label>MODEL</label>
+                        {boatData.vesselModel}
+                    </div>
+                    <div className="form-row">
+                        <label>BEAM</label>
+                        {boatData.vesselBeam}
+                    </div>
+                    <div className="form-row">
+                        <label>BERTH</label>
+                        {boatData.vesselBerth}
+                    </div>
+                </div>
+                <div className="form-column">
+                    <div className="form-row">
+                        <label>PRICE</label>
+                        {boatData.vesselPrice}
+                    </div>
+                    <div className="form-row">
+                        <label>DRAFT</label>
+                        {boatData.vesselDraft}
+                    </div>
+                </div>
+                <div className="form-column">
+                    <div className="form-row">
+                        <label>YEAR</label>
+                        {boatData.vesselYear}
+                    </div>
+                    <div className="form-row">
+                        <label>KEEL TYPE</label>
+                        {boatData.vesselKeelType}
+                    </div>
+                </div>
+                <div className="form-column">
+                    <div className="form-row">
+                        <label>COUNTRY</label>
+                        {boatData.vesselLocationCountry}
+                    </div>
+                    <div className="form-row">
+                        <label>FUEL TYPE</label>
+                        {boatData.vesselFuelType}
+                    </div>
+                </div>
+                <div className="form-column">
+                    <div className="form-row">
+                        <label>STATE</label>
+                        {boatData.vesselLocationState}
+                    </div>
+                    <div className="form-row">
+                        <label>ENGINE QTY</label>
+                        {boatData.engineQuantity}
                     </div>
                 </div>
             </div>
