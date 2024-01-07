25435641dda6f659c5c4706dbb13ef02cb22a49f
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2024-01-07 16:06:23 +0000

    Fix Mailgun creds error 3.0

--- backend/src/main/java/com/norwayyachtbrockers/controler/ContactController.java
+++ backend/src/main/java/com/norwayyachtbrockers/controler/ContactController.java
@@ -4,11 +4,13 @@ import com.norwayyachtbrockers.model.ContactForm;
 import com.norwayyachtbrockers.service.EmailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/contact")
 public class ContactController {
