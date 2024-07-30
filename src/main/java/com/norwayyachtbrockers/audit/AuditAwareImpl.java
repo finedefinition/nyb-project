cb7bf2e86a21d77ce66f8d1cfb2a4b39838e35e4
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2024-07-30 16:23:43 +0100

    Implement auto deploy on AWS server v 10.0

--- src/main/java/com/norwayyachtbrockers/audit/AuditAwareImpl.java
+++ src/main/java/com/norwayyachtbrockers/audit/AuditAwareImpl.java
@@ -15,7 +15,7 @@ public class AuditAwareImpl implements AuditorAware<String> {
      */
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("SERGII BEZRUKOV");
+        return Optional.of("SERGII");
     }
 }
 
