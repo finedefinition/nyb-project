0e8f4894377bdeb9bbcff17a275e2ce40dd7b3ce
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2024-08-12 15:09:16 +0100

    Test push

--- src/main/java/com/norwayyachtbrockers/validation/annotations/ValidUserRole.java
+++ src/main/java/com/norwayyachtbrockers/validation/annotations/ValidUserRole.java
@@ -1,9 +1,7 @@
 package com.norwayyachtbrockers.validation.annotations;
 
-
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
