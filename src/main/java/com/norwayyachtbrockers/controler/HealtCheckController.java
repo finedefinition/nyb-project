a62267c33d8308fff0b464fae304e0a91b534dab
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2024-11-17 21:36:07 +0000

    Fix deploy-target-group-test-backend.yml v 1.2

--- src/main/java/com/norwayyachtbrockers/controler/HealtCheckController.java
+++ src/main/java/com/norwayyachtbrockers/controler/HealtCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/version")
 public class HealtCheckController {
 
     @GetMapping
