849e90327fd36d3cca1c6f49081492695ce7fb89
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2024-11-17 22:13:40 +0000

    Fix deploy-target-group-test-backend.yml v 1.4

--- src/main/java/com/norwayyachtbrockers/controler/HealtCheckController.java
+++ src/main/java/com/norwayyachtbrockers/controler/HealtCheckController.java
@@ -11,6 +11,6 @@ public class HealtCheckController {
 
     @GetMapping
     public ResponseEntity healthCheck() {
-        return ResponseEntity.ok("v 1.0.2");
+        return ResponseEntity.ok("v 1.0.3");
     }
 }
